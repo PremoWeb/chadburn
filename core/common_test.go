580ed3bad82a6a4fe3da655533445b91abfd44ea
Nick Maietta <nick@maietta.org>
2021-10-01 18:11:53 -0700

    Re-added to see if it fails again

--- core/common_test.go
+++ core/common_test.go
@@ -215,7 +215,7 @@ func (s *SuiteCommon) TestExecutionStopError(c *C) {
 	c.Assert(exe.Failed, Equals, true)
 	c.Assert(exe.Skipped, Equals, false)
 	c.Assert(exe.Error, Equals, err)
-
+	c.Assert(exe.Duration.Seconds() > .0, Equals, true)
 }
 
 func (s *SuiteCommon) TestExecutionStopErrorSkip(c *C) {
