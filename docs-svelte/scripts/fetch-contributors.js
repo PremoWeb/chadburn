c6be8724fe9512d9f04da31dc3d1edb6047c30c8
Nick Maietta <nick@premoweb.com>
2025-03-14 19:52:43 -0700

    fix: update contributors script to use ES modules syntax

--- docs-svelte/scripts/fetch-contributors.js
+++ docs-svelte/scripts/fetch-contributors.js
@@ -5,8 +5,13 @@
  * It's meant to be run by the GitHub Action when new commits are pushed
  */
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function fetchContributors() {
     try {
